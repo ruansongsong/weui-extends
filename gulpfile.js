e5651b2fb5981983b641bb51241a5f2ddea848fb
ruansongsong <ruansongsong@gmail.com>
2016-07-25 22:11:28 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var  sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var header = require('gulp-header');
+var pkg = require('./package.json');
+
+var banner = 
+"/**\n\
+* weui extends v" + pkg.version + "\n\
+* author ruansongsong\n\
+*/\n";
+gulp.task('less', function () {
+	gulp.src('src/weui_extend.less')
+	.pipe(sourcemaps.init())
+	.pipe(less())
+	.pipe(postcss([autoprefixer(['ios >= 7', 'android >= 4.1'])]))
+	.pipe(header(banner))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('dist/style'))
+	.pipe(browserSync.reload({stream: true}));
+});
+gulp.task('watch', function() {
+    gulp.watch('src/style/**/*', ['less']);
+});
+gulp.task('server', function () {
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		}
+	});
+});
+gulp.task('test', function () {
+	gulp.start('server');
+	gulp.start('watch');
+});
